117337229fe84323d889c196b0834ccd8398e0f6
salmanpot <asghersalmaan@gmail.com>
2023-02-01 13:40:15 +0500

    Hardhat configurations

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,33 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('@openzeppelin/hardhat-upgrades');
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const {
+  BSC_TESTNET_URL,
+  BSC_TESTNET_DEPLOY_KEY,
+  BSCSCAN_API_KEY,
+} = require("./env.json")
+
 module.exports = {
-  solidity: "0.8.17",
-};
+  networks: {
+    bscTestnet: {
+      url: BSC_TESTNET_URL,
+      chainId: 97,
+      gasPrice: 20000000000,
+      accounts: [BSC_TESTNET_DEPLOY_KEY]
+    }
+  },
+  etherscan: {
+    apiKey: {
+      bscTestnet: BSCSCAN_API_KEY
+    }
+  },
+  solidity: {
+    version: "0.8.0",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1
+      }
+    }
+  }
+}
