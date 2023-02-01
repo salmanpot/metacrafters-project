c62b919c82239a0fef6314b6d281973073991d68
salmanpot <asghersalmaan@gmail.com>
2023-02-01 13:48:09 +0500

    Deployment scripts

---
+++ scripts/deploy.js
@@ -0,0 +1,16 @@
+// scripts/create-box.js
+const { ethers, upgrades } = require("hardhat");
+
+const owner = "";
+const token = "";
+const fundingGoal = 0;
+
+async function main() {
+  const crowdFunding = await ethers.getContractFactory("CrowdFunding");
+  const CrowdFunding = await crowdFunding.deploy(owner, token, fundingGoal);
+  await CrowdFunding.deployed();
+  console.log("CrowdFunding deployed to:", CrowdFunding.address);
+  
+}
+
+main();
